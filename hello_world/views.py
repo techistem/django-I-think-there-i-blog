cc893f141a21a5b8c628fd69ad0854b69432141a
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-09-16 13:59:22 +0000

    Add post

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def about_me(request):
-    return HttpResponse("This would be the about page")
 
 def index(request):
-    return HttpResponse("Welcome to the hello world page")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
