8424a42e22fc54840bfe498de37d62566adc1eed
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-09-16 13:04:48 +0000

    Add view and about function url link

---
+++ hello_world/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def about_me(request):
+    return HttpResponse("This would be the about page")
+
+def index(request):
+    return HttpResponse("Welcome to the hello world page")
---
+++ hello_worldvvvvvvvvvv/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HelloWorldvvvvvvvvvvConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hello_worldvvvvvvvvvv'
