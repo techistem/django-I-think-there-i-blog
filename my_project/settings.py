a95b396422072b06236065a5343450d6b4030746
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-11-03 14:34:05 +0000

    Deploy to Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-techistem-djangoithinkt-5ufsqewvc52.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-techistem-djangoithinkt-5ufsqewvc52.ws.codeinstitute-ide.net'
+                ,'.herokuapp.com']
 
 
 # Application definition
