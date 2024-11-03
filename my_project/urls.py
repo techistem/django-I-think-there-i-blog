3eb60e1f88a7b7aaf50d0d05df2bc9593957e237
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-11-03 17:51:38 +0000

    Add django-summernote for rich text editing in admin panel

--- my_project/urls.py
+++ my_project/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("blog/", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
 
 
