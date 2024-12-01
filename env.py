3bda697d5e4efc18b91669f552aca4424d159527
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-12-01 17:53:50 +0000

    enable serving of image files

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:zSjrJT9cNgF1@ep-falling-darkness-a22ln895.eu-central-1.aws.neon.tech/slate_tree_bust_115469")
 
 os.environ.setdefault("SECRET_KEY", "<3656537isT>")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<881397972213959>:<Cn2kj4aX2P1x6s3an7qaankOvPQ>@drbyc4bof")
--- my_project/settings.py
+++ my_project/settings.py
@@ -29,8 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['8000-techistem-djangoithinkt-5ufsqewvc52.ws.codeinstitute-ide.net'
                 ,'.herokuapp.com']
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
